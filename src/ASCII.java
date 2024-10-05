17530f0675bc525f78c4f55d89761a3dabbcc758
kjone8371 <rlawjddnsj8371@gmail.com>
2024-10-05 15:32:19 +0900

    아스키코드

---
+++ src/ASCII.java
@@ -0,0 +1,16 @@
+public class ASCII {
+    public static void main(String[] args) {
+        int ab = 'Z';
+
+        System.out.println(ab);
+
+
+        //아스키코드 변환
+        char c = 'A';
+        int c1 = 'A';
+
+        System.out.println((int)c);
+        System.out.println((int)'A');
+        System.out.println(c1);
+    }
+}
